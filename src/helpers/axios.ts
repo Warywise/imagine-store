2f525432ebc55c5fc87132576c01db06a5a1c15c
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-28 00:08:39 -0300

    Create(context)=> implements helper function 'axiosRefreshToken'

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -4,15 +4,45 @@ import { LoginReturn } from '../interfaces/auth';
 const URL = 'http://localhost:3001';
 
 type LoginData = { email: string, hash: string };
+type GetUserData = { email: string, token: string };
 
-export async function axiosLogin(data: LoginData): Promise<LoginReturn> {
+export async function axiosLogin({ email, hash }: LoginData): Promise<LoginReturn> {
   try {
     return await axios({
       method: 'POST',
       url: `${URL}/auth/login`,
-      data,
-      responseType: 'json'
+      data: { email, hash },
+      timeout: 10000,
     }).then((response) => response.data) as LoginReturn;
+  } catch (error) {
+    const { response } = error as AxiosError;
+    return response?.data;
+  }
+}
+
+export async function axiosGetUser({ email, token: authorization }: GetUserData) {
+  try {
+    return await axios({
+      method: 'GET',
+      url: `${URL}/users`,
+      headers: { authorization, email },
+      timeout: 10000,
+    }).then((response) => response.data);
+  } catch (error) {
+    const { response } = error as AxiosError;
+    return response?.data;
+  }
+}
+
+export async function axiosRefreshToken({ email, token: authorization }: GetUserData) {
+  try {
+    return await axios({
+      method: 'POST',
+      url: `${URL}/auth/refresh`,
+      headers: { authorization },
+      data: { email },
+      timeout: 10000,
+    }).then((response) => response.data);
   } catch (error) {
     const { response } = error as AxiosError;
     return response?.data;
