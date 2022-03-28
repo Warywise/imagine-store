7fe49f56062a8fa7db83238ebc73ef0a343f52c3
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-28 00:12:14 -0300

    Implement(context)=> implements useEffects: - verify token onload page - refresh token at interval if user is active

--- src/context/mainContext.tsx
+++ src/context/mainContext.tsx
@@ -1,9 +1,10 @@
 import React, {
   createContext, Dispatch, SetStateAction, useEffect, useState,
 } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
-import { getCookie } from '../helpers/cookie';
+import { axiosGetUser, axiosRefreshToken } from '../helpers/axios';
+import { destroyCookie, getCookie, setCookie } from '../helpers/cookie';
 
 import { PropChild } from '../interfaces/default';
 
@@ -23,21 +24,61 @@ export const MainContext = createContext({} as InitialContext);
 
 export function MainProvider({ children }: PropChild) {
   const [active, setActive] = useState(false);
-  const [currentUser, setCurrentUser] = useState({} as InitialUser);
-  const navigateTo = useNavigate();
-  
-  useEffect(() => {
+  const [currentUser, setCurrentUser] = useState({ email: '', name: '' });
+  // const navigateTo = useNavigate();
+  const TEN_MINUTES = 1000 * 60 * 10;
+
+  const getUserData = async () => {
+    const auth = getCookie('auth_handler');
+    if (auth)  return await axiosGetUser(auth);
+    return null;
+  };
+
+  const refreshToken = async () => {
     const auth = getCookie('auth_handler');
     if (auth) {
-      // axiosRefreshToken(auth)
-      // axiosGetUser(auth)
-      // setCurrentUser(user)
-      // setActive(true);
-      console.log(auth);
-      return navigateTo('/', { replace: true });
+      if (currentUser.email !== auth.email) {
+        destroyCookie('auth_handler');
+        return setActive(false);
+      }
+
+      const userData = await axiosRefreshToken(auth);
+      if (userData.active) {
+        const { email, name, token } = userData;
+        setCookie('auth_handler', token);
+        return setCurrentUser({ email, name });
+      }
+
+      destroyCookie('auth_handler');
+      return setActive(false);
     }
+  };
+
+  const refreshTokenInterval = () => {
+    if (active) {
+      setTimeout(async () => {
+        await refreshToken();
+        refreshTokenInterval();
+      }, TEN_MINUTES);
+    }
+  };
+
+  const verifyToken = async () => {
+    const userResult = await getUserData();
+    if (userResult.active) {
+      const { email, name } = userResult;
+      setCurrentUser({ email, name });
+      setActive(true);
+    }
+  };
+
+  useEffect(() => {
+    verifyToken();
   }, []);
-  
+
+  useEffect(() => {
+    refreshTokenInterval();
+  }, []);
 
   const contextValue = {
     active,
