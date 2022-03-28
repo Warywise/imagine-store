d1f6487e34c74c9ae78db11694f50a713363b137
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-28 16:20:30 -0300

    Implement(context)=> implements getCategories  StoreContext

--- src/context/storeContext.tsx
+++ src/context/storeContext.tsx
@@ -1,18 +1,48 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { axiosGetter } from '../helpers/axios';
 
 import { PropChild } from '../interfaces/default';
+
+type INITIAL_VALUE = Record<string, unknown>[];
 
 export const StoreContext = createContext({});
 
 export function StoreProvider({ children }: PropChild) {
+  const [products, setProducts] = useState([] as INITIAL_VALUE);
+  const [categories, setCategories] = useState([] as INITIAL_VALUE);
+
+  const getCategories = async () => {
+    const categoriesResult = await axiosGetter('/categories');
+    if (Array.isArray(categoriesResult)) {
+      setCategories(categoriesResult);
+    }
+  };
+
+  const getProducts = async () => {
+    //
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  useEffect(() => {
+    // getProducts();
+  }, []);
+
+  const storeValue = {
+    products,
+    categories,
+  };
+
   return (
-    <StoreContext.Provider value={{}}>
+    <StoreContext.Provider value={storeValue}>
       { children }
     </StoreContext.Provider>
   );
 }
 
-export function useStoreProvider({ children }: PropChild) {
+export function useStoreProvider(children: JSX.Element) {
   return (
     <StoreProvider>
       { children }
