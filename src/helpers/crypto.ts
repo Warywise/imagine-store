c6847847da0f672e6ed96dbbd3521edd7f09e8bd
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-28 21:16:28 -0300

    Implement(context)=> implements 'cart' and 'favorites' at UserContext

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -1,6 +1,7 @@
 import CryptoJS from 'crypto-js';
+import { ProductType } from '../interfaces/store';
 
-export const encrypt = (payload: Record<string, unknown>[]) =>
+export const encrypt = (payload: ProductType[]) =>
   CryptoJS.AES.encrypt(JSON.stringify(payload), 'secret').toString();
 
 export const decrypt = (payload: string) => {
