d5b1d7e5544ab2d0f07d2dd4070c1a9498a8b7ca
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-29 04:05:14 -0300

    Style(header)=> basic styles for CategoriesDropdown

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { BsCart4 } from 'react-icons/bs';
+import { HiOutlineMenu } from 'react-icons/hi';
 
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Container from 'react-bootstrap/Container';
@@ -19,11 +20,13 @@ function Header() {
   const defaultS = { textDecoration: 'none' };
 
   return (
-    <Navbar bg="dark" expand={false} sticky='top' collapseOnSelect>
+    <Navbar bg="dark" expand={false} sticky='top' collapseOnSelect className='nav-bar'>
       <Container fluid>
-        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Toggle aria-controls="offcanvasNavbar" className='nav-toggle border-light'>
+          <HiOutlineMenu className='nav-toggle-icon'/>
+        </Navbar.Toggle>
         <Navbar.Brand href="#"><h2>Imagine Store</h2></Navbar.Brand>
-        <BsCart4 color='white' size={40} />
+        <BsCart4 color='white' size={35} />
         <Navbar.Offcanvas
           id="offcanvasNavbar"
           aria-labelledby="offcanvasNavbarLabel"
