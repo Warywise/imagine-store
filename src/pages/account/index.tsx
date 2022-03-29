bd5b62e8cf41b887a509b754ff74edb0dc1a9c8c
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-29 15:32:14 -0300

    Create(pages)=> create Account Page

---
+++ src/pages/account/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+
+export default function Account() {
+  return (
+    <div>
+      <Header />
+      <h1>Account Page</h1>
+    </div>
+  );
+}
