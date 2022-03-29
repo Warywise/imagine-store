b777f9df3b20ee3fcd8782b621eeec5f79996cf4
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-29 16:41:10 -0300

    Updates(mainPage)=> init MainPage implementations

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,22 +2,16 @@ import React, { useState, useEffect, useContext } from 'react';
 
 import Header from '../../components/header/Header';
 
-import { MainContext } from '../../context/mainContext';
+// import { MainContext } from '../../context/mainContext';
 import { useStoreProvider } from '../../context/storeContext';
 
 function Main() {
-  const { currentUser: user } = useContext(MainContext);
+  // const { } = useContext(MainContext);
 
   return (
     <>
       <Header />
       <h1>Main</h1>
-      <p>
-        { `Email: ${user.email}` }
-      </p>
-      <p>
-        { `Name: ${user.name}` }
-      </p>
     </>
   );
 }
