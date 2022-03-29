72b1c06ec5ecebe59c45574e8d9d9be30b034ddf
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-29 15:35:04 -0300

    Update(accountPage)=> adds an condition to redirect to Login

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import Header from '../../components/header/Header';
+import { MainContext } from '../../context/mainContext';
 
 export default function Account() {
+  const { active } = useContext(MainContext);
+  const navigateTo = useNavigate();
+
+  useEffect(() => {
+    if (!active) navigateTo('/auth/signin', { replace: true });
+  }, [active]);
+
   return (
     <div>
       <Header />
