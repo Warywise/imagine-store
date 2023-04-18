2eac72a36621dc07f3cd5318a4bfc59088860722
Gustavo Sant'Anna <g_santanna@outlook.com>
2023-04-18 16:39:20 -0300

    feat(pagination): add pagination component and some improvements   - implements filter options on GET requests   - fix some style issues   - improve StoreContext to handle pagination

--- src/context/storeContext.tsx
+++ src/context/storeContext.tsx
@@ -1,15 +1,19 @@
 import React, { createContext, Dispatch, SetStateAction, useContext, useEffect, useState } from 'react';
 import { axiosGetter } from '../helpers/axios';
 
-import { PropChild } from '../interfaces/default';
+import { anyType, PropChild } from '../interfaces/default';
 import { CategoryType, ProductType } from '../interfaces/store';
 import { MainContext } from './mainContext';
 
 type StoreType = {
   products: ProductType[],
-  allProducts: ProductType[],
   setProducts: Dispatch<SetStateAction<ProductType[]>>,
   categories: CategoryType[],
+  query: string,
+  setQuery: Dispatch<SetStateAction<string>>,
+  setPage: Dispatch<SetStateAction<number>>,
+  total: number,
+  limit: number,
 };
 
 export const StoreContext = createContext({} as StoreType);
@@ -17,37 +21,52 @@ export const StoreContext = createContext({} as StoreType);
 export function StoreProvider({ children }: PropChild) {
   const { categories, categoryFilter } = useContext(MainContext);
 
-  const [allProducts, setAllProducts] = useState([] as ProductType[]);
   const [products, setProducts] = useState([] as ProductType[]);
+  const [total, setTotal] = useState(0);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+
+  const limit = 20;
 
   const getProducts = async () => {
-    const productsResult: ProductType[] = await axiosGetter('/products');
-    if (Array.isArray(productsResult)) {
-      setAllProducts(productsResult);
+    const skip = (page - 1) * limit;
+    const filter = { take: 20, skip, orderBy: { name: 'asc' } } as { [key: string]: anyType };
+    if (query) {
+      filter.name = query;
+    }
+
+    const result = await axiosGetter('/products/query', filter) as { products?: ProductType[], total: number };
+    if (result.products) {
+      setProducts(result.products);
+      setTotal(result.total);
     }
   };
 
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [page, query]);
 
   useEffect(() => {
-    if (allProducts.length > 0) {
-      if (categoryFilter === '') {
-        setProducts(allProducts);
-      } else {
-        const productsToRender = allProducts
-          .filter(({ category }) => category.name.includes(categoryFilter));
-        setProducts(productsToRender);
-      }
-    }
-  }, [allProducts, categoryFilter]);
+    // if (allProducts.length > 0) {
+    //   if (categoryFilter === '') {
+    //     setProducts(allProducts);
+    //   } else {
+    //     const productsToRender = allProducts
+    //       .filter(({ category }) => category.name.includes(categoryFilter));
+    //     setProducts(productsToRender);
+    //   }
+    // }
+  }, [categoryFilter]);
 
   const storeValue = {
     products,
-    allProducts,
     setProducts,
+    query,
+    setQuery,
     categories,
+    setPage,
+    total,
+    limit,
   };
 
   return (
